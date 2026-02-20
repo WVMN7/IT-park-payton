91c282a8f566bd3b789f7b1f9f34b4fd59ade6c2
WVMN <abbossabiryozov6@gmail.com>
2026-02-20 14:43:09 +0500

    Add files via upload

---
+++ matth.py
@@ -0,0 +1,15 @@
+#a = int(input("uchburchakning birinchi tomonini kriting:"))
+#b = int(input("uchburchakning ikkinchi tomonini kriting:"))
+#c = int(input("uchburchakning uchinchi tomonini kriting:"))
+#javob = a + b + c
+#print(f'uchburchakning peremetri {javob} sm ga teng')
+'''
+son = int(input('sonni kriting :'))
+son1 = int(input('darajani kriting:'))
+natija = son ** son1
+print(f"{son} darajasi {son1} ga teng javob {natija}")
+'''
+# import calendar
+# yy = 2024
+# mm = 11
+# print(calendar.month(yy ,mm))
